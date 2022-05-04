dd698c18a9efc909829b589b4d19956933b25eac
Edgar <franky9202@gmail.com>
2022-05-03 18:01:45 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Document is ready to be manipulated")
+    document.getElementById("text").textContent = "This is really cool!"
+}) 
